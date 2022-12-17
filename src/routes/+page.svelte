87d4afe3bfc5a328ff0c8f7f713039da61677aed
Oleg K <oleg.kokoshinskii@gmail.com>
2022-12-17 20:47:55 +0600

    [test-task]: add PictureViewers to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
+<script>
+	import images from '$lib/data/image_data.json';
+	import PicturesViewer from '$lib/Components/PicturesViewer/PicturesViewer.svelte';
+</script>
 
+<!-- <h1>This text needs to replaced with a component showing Airbnb style image grid</h1>
+<p>
+	See <a href="https://www.airbnb.com/rooms/20238974">the link</a> for an example. The ideal component
+	will make use of Flowbite-Svelte and Tailwind CSS
+</p>
+<p>
+	The grid should be dynamic and interactive and work for multiple screen sizes similiar to the one
+	on Airbnb
+</p>
+<p>Example images can be found in image_data.json</p> -->
 
-<h1>This text needs to replaced with a component showing Airbnb style image grid</h1>
-<p>See <a href="https://www.airbnb.com/rooms/20238974">the link</a> for an example. The ideal component will make use of Flowbite-Svelte and Tailwind CSS</p>
-<p>The grid should be dynamic and interactive and work for multiple screen sizes similiar to the one on Airbnb</p>
-<p>Example images can be found in image_data.json</p>
-
+<div class="max-w-[95%] mx-auto">
+  {#each images as imagesPack} 
+	<PicturesViewer images={imagesPack.images} />
+  {/each}
+</div>
